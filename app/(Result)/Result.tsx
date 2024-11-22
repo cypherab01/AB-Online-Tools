5bd5f3d1083f3d91e889e6f48a8347b74f43e1d5
cypherab01 <aghimire074@gmail.com>
2024-11-22 08:25:35 +0545

    feat: button to download and clear text

--- app/(Result)/Result.tsx
+++ app/(Result)/Result.tsx
@@ -30,6 +30,29 @@ const ResultUI = () => {
     characterCount: handleCharacterCount(),
   };
 
+  const downloadText = () => {
+    if (data) {
+      const blob = new Blob([data], { type: "text/plain" });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      const filenameParts = data.trim().split(/\s+/).slice(0, 2);
+      const formattedFilename =
+        filenameParts
+          .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+          .join(" ") || "Text_data";
+      a.download = `${formattedFilename}.txt`;
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      URL.revokeObjectURL(url);
+    }
+  };
+
+  const clearText = () => {
+    setData("");
+  };
+
   return (
     <>
       <div className="py-4 text-3xl">{`${
@@ -43,7 +66,25 @@ const ResultUI = () => {
         onChange={(e) => setData(e.target.value)}
         rows={10}
         className="border-2 outline-2 border-sky-300 w-full p-2 rounded-md focus:outline-sky-500"
+        value={data}
       ></textarea>
+      <div className="flex items-center justify-start gap-4">
+        <button
+          onClick={downloadText}
+          className={`mt-4 ${
+            !data ? "bg-gray-400 cursor-not-allowed" : "bg-sky-500"
+          } text-white p-2 rounded-md`}
+          disabled={!data}
+        >
+          Download Text
+        </button>
+        <button
+          onClick={clearText}
+          className="mt-4 bg-red-500 text-white p-2 rounded-md"
+        >
+          Clear Text
+        </button>
+      </div>
     </>
   );
 };
